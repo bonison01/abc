b2b52f317c085fad6baf697e7bdaaf2e3b81c8f9
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 00:24:12 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,26 +1,25 @@
 import React from "react";
+import Link from "next/link";
+import { Content } from "./content";
 
-export function GoogleSheetsEmbed() {
-  const handleButtonClick = () => {
-    // Define your button click logic here
-    alert("Button clicked!");
-  };
-
+export function AboutsMain() {
   return (
-    <div className="text-center pt-20 pb-10 sm:pt-32">
-      <iframe
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-        width="100%"
-        height="400"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-      <div className="mt-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleButtonClick}
-        >
-          Click Me
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      <div className="border border-gray-400 rounded-lg p-4">
+        {/* Google Sheets iframe */}
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
+          width="100%"
+          height="400"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+
+        {/* Button to navigate to "/test" */}
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/test">
+            <a>Test</a>
+          </Link>
         </button>
       </div>
     </div>
