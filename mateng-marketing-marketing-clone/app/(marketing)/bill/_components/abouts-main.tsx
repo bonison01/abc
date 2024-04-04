1619c657278065c7678b20aae13f15f359a101d8
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 23:50:01 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -7,8 +7,8 @@ export function AboutsMain() {
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       <div className="border border-gray-400 rounded-lg p-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          <Link href="/delivery-rates">
-            <a>Test</a>
+          <Link href="/contact-us">
+            <a>contact</a>
           </Link>
         </button>
       </div>
