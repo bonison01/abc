67a122cc05843a9b71fab8a4b3a5b8ab4d2ea8a0
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 00:10:08 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -8,7 +8,21 @@ export function AboutsMain() {
       <div className="border border-gray-400 rounded-lg p-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <Link href="/padding_order">
-            <a>contact</a>
+            <a>Contact 1</a>
+          </Link>
+        </button>
+      </div>
+      <div className="border border-gray-400 rounded-lg p-4">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/contact-us">
+            <a>Contact 2</a>
+          </Link>
+        </button>
+      </div>
+      <div className="border border-gray-400 rounded-lg p-4">
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/delivery-rates">
+            <a>Contact 3</a>
           </Link>
         </button>
       </div>
