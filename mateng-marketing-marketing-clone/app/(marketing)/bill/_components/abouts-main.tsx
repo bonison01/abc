72016fdfa490aeccffd57595c0ba2a86f6d9c9ca
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 22:15:52 +0530

    Add files via upload

---
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { Content } from "./content";
+
+export function AboutsMain() {
+ 
+  return (
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      OKAY TEST
+    </div>
+  );
+}
