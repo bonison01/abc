8131d4d6ecbc9bfe2e736c3a7b8367862613634f
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:36:37 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,11 +1,32 @@
+import React from "react";
 import Link from "next/link";
-import { Content } from "./content";
 
 export function AboutsMain() {
- 
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      OKAY TEST
+      <h1>Welcome to AboutsMain Component</h1>
+      <p>This is a test component.</p>
+
+      {/* Embedded Google Sheets iframe */}
+      <div className="border border-gray-400 rounded-lg p-4 mt-8">
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
+          width="100%"
+          height="400"
+          frameBorder="0"
+          allowFullScreen
+          title="Embedded Google Sheets"
+        ></iframe>
+      </div>
+
+      {/* Button to navigate to "/test" */}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/test">
+          <a>Test</a>
+        </Link>
+      </button>
     </div>
   );
 }
+
+export default AboutsMain;
