d5203e1daa5e009030ab71ebe2fba9faf8221c60
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 10:15:21 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,12 +1,30 @@
+import React from "react";
 import Link from "next/link";
-import { Content } from "./content";
 
 export function AboutsMain() {
-  
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      {/* Embed the iframe here */}
-      <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml" width="100%" height="400"></iframe>
+      {/* Embedded Google Sheets iframe */}
+      <div className="w-full max-w-screen-lg mx-auto mt-8">
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
+          width="100%"
+          height="600" // Adjust the height as needed
+          frameBorder="0"
+          allowFullScreen
+          title="Embedded Google Sheets"
+          style={{ minHeight: "400px" }} // Set minimum height for responsiveness
+        ></iframe>
+      </div>
+
+      {/* Optional: Add a button or link */}
+      <Link href="/test">
+        <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Test
+        </a>
+      </Link>
     </div>
   );
 }
+
+export default AboutsMain;
