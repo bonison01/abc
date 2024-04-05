62f24ab927a7ff06cd1b80d5aff2af736ef1a939
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 08:23:04 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -5,10 +5,13 @@ import { Content } from "./content";
 export function AboutsMain() {
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      <div>
+      <div className="w-full overflow-x-auto">
         {/* Google Sheets iframe */}
         <iframe
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
+          className="w-full h-screen"  // Adjust width and height accordingly
+          style={{ border: "none" }}   // Hide iframe border
+          sandbox="allow-scripts allow-same-origin"
         ></iframe>
       </div>
       <div>
