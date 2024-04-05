3c276c60f9e6b496646bca764b40db1fa8ef0f9b
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:20:18 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -4,20 +4,20 @@ import { Content } from "./content";
 
 export function AboutsMain() {
   return (
-    <div>
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       {/* Button to navigate to "/test" */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         <Link href="/test">
           <a>Test</a>
         </Link>
       </button>
-    </div>
-    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+
       {/* Google Sheets iframe */}
-      <div className="border border-gray-400 rounded-lg p-4 w-full max-w-screen-lg">
+      <div className="border border-gray-400 rounded-lg p-4 mt-8">
         <iframe
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-          className="w-full h-96" // Set height as needed
+          width="100%" // Set the width to 100% of the container
+          height="400" // Set the desired height
           style={{ border: "none" }} // Hide iframe border
           sandbox="allow-scripts allow-same-origin"
         ></iframe>
