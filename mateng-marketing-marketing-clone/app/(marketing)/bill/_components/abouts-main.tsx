156fdc9c0d111a5d9b9b5383d865c6aab74213e2
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:08:39 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -5,23 +5,22 @@ import { Content } from "./content";
 export function AboutsMain() {
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      <div className="w-full overflow-x-auto">
-        {/* Google Sheets iframe */}
+      {/* Google Sheets iframe */}
+      <div className="border border-gray-400 rounded-lg p-4 w-full h-screen">
         <iframe
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-          className="w-full h-screen"  // Adjust width and height accordingly
-          style={{ border: "none" }}   // Hide iframe border
+          className="w-full h-full"
+          style={{ border: "none" }} // Hide iframe border
           sandbox="allow-scripts allow-same-origin"
         ></iframe>
       </div>
-      <div>
-        {/* Button to navigate to "/test" */}
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          <Link href="/test">
-            <a>Test</a>
-          </Link>
-        </button>
-      </div>
+
+      {/* Button to navigate to "/test" */}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/test">
+          <a>Test</a>
+        </Link>
+      </button>
     </div>
   );
 }
