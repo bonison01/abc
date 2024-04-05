5faf33507261a8f1ad1231063570c2c5ba564251
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:51:50 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,33 +1,11 @@
-import React from "react";
 import Link from "next/link";
+import { Content } from "./content";
 
 export function AboutsMain() {
+ 
   return (
-    <div>
-      <h1>Welcome to AboutsMain Component</h1>
-      <p>This is a test component.</p>
-
-      {/* Embedded Google Sheets iframe */}
-      <div className="w-full max-w-screen-lg mx-auto">
-        <iframe
-          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-          width="100%"
-          height="600" // Adjust the height as needed
-          frameBorder="0"
-          allowFullScreen
-          title="Embedded Google Sheets"
-          style={{ width: "100%", maxWidth: "100%" }}
-        ></iframe>
-      </div>
-
-      {/* Button to navigate to "/test" */}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <Link href="/test">
-          <a>Test</a>
-        </Link>
-      </button>
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      OKAY TEST
     </div>
   );
 }
-
-export default AboutsMain;
