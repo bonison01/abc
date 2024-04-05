8ccdd2d336a89966543bda238a2a207c5551b2ed
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 10:13:34 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,14 +1,12 @@
-import React from "react";
+import Link from "next/link";
+import { Content } from "./content";
 
 export function AboutsMain() {
+  
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      <h1>OKAY TEST</h1>
-      {/* Embedded Google Sheets iframe */}
       {/* Embed the iframe here */}
       <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml" width="100%" height="400"></iframe>
     </div>
   );
 }
-
-export default AboutsMain;
