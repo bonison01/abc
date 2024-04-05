899b1886ab88ff706d1d6a58392a6fa153e201a2
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 10:10:10 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -5,16 +5,6 @@ export function AboutsMain() {
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       <h1>OKAY TEST</h1>
       {/* Embedded Google Sheets iframe */}
-      <div className="w-full max-w-screen-lg mx-auto mt-8">
-        <iframe
-          src="https://docs.google.com/spreadsheets/d/e/your-spreadsheet-id/pubhtml?widget=true&amp;headers=false"
-          width="100%"
-          height="600"
-          frameBorder="0"
-          allowFullScreen
-          title="Embedded Google Sheets"
-        ></iframe>
-      </div>
       {/* Embed the iframe here */}
       <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml" width="100%" height="400"></iframe>
     </div>
