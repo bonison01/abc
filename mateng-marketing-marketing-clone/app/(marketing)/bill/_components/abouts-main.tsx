c4196a545ef71e4bd8c80919be32fdb9c84798ca
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:12:04 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -4,7 +4,7 @@ import { Content } from "./content";
 
 export function AboutsMain() {
   return (
-    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+    <div className="text-center pt-30 pb-50 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       {/* Google Sheets iframe */}
       <div className="border border-gray-400 rounded-lg p-4 w-full h-screen">
         <iframe
