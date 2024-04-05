1b3b28ccaf2679a970d8183569d1eccb4f61f600
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:54:26 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/about-us/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/about-us/_components/abouts-main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import Link from "next/link";
+import { Content } from "./content";
 
 export function AboutsMain() {
   const instagram = process.env.INSTAGRAM_PAGE || "#";
@@ -9,43 +9,36 @@ export function AboutsMain() {
 
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      {/* Content component */}
       <Content />
-
-      {/* Text description */}
       <div className="font-semibold text-lg sm:text-xl w-[70%] sm:pt-10">
         Learn more about our impactful work at Mateng.
       </div>
-
-      {/* Social media links */}
       <div className="flex">
         <Link href={facebook}>
-          <img src="/facebook.png" alt="Facebook" className="h-10 w-10 mr-4" />
+          <img
+            src={"/facebook.png"}
+            alt="facebook"
+            className="h-10 w-10 mr-4"
+          />
         </Link>
         <Link href={instagram}>
-          <img src="/instagram.png" alt="Instagram" className="h-10 w-10 mr-4" />
+          <img
+            src={"/instagram.png"}
+            alt="instagram"
+            className="h-10 w-10 mr-4"
+          />
         </Link>
         <Link href={linkedIn}>
-          <img src="/linkedin.png" alt="LinkedIn" className="h-10 w-10 mr-4" />
+          <img
+            src={"/linkedin.png"}
+            alt="linkedin"
+            className="h-10 w-10 mr-4"
+          />
         </Link>
         <Link href={youtube}>
-          <img src="/youtube.png" alt="YouTube" className="h-10 w-10 mr-4" />
+          <img src={"/youtube.png"} alt="youtube" className="h-10 w-10 mr-4" />
         </Link>
-      </div>
-
-      {/* Embedded Google Sheets iframe */}
-      <div className="w-full max-w-screen-lg mx-auto mt-8">
-        <iframe
-          src="https://docs.google.com/spreadsheets/d/e/your-spreadsheet-id/pubhtml?widget=true&amp;headers=false"
-          width="100%"
-          height="600"
-          frameBorder="0"
-          allowFullScreen
-          title="Embedded Google Sheets"
-        ></iframe>
       </div>
     </div>
   );
 }
-
-export default AboutsMain;
