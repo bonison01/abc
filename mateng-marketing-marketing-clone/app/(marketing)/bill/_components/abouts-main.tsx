c7444819a8ee05d758e12e5e31a243d73c7d26d3
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:33:21 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,44 +1,11 @@
-import React, { useEffect, useRef } from "react";
 import Link from "next/link";
+import { Content } from "./content";
 
 export function AboutsMain() {
-  const iframeRef = useRef(null);
-
-  useEffect(() => {
-    if (iframeRef.current) {
-      const iframe = iframeRef.current;
-      const resizeIframe = () => {
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
-      };
-      iframe.onload = resizeIframe;
-      window.addEventListener("resize", resizeIframe);
-      return () => {
-        window.removeEventListener("resize", resizeIframe);
-      };
-    }
-  }, []);
-
+ 
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      {/* Button to navigate to "/test" */}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        <Link href="/test">
-          <a>Test</a>
-        </Link>
-      </button>
-
-      {/* Embedded Google Sheets iframe */}
-      <div className="border border-gray-400 rounded-lg p-4 mt-8 w-full max-w-screen-lg">
-        <iframe
-          ref={iframeRef}
-          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-          width="100%"
-          height="400"
-          frameBorder="0"
-          allowFullScreen
-          title="Embedded Google Sheets"
-        ></iframe>
-      </div>
+      OKAY TEST
     </div>
   );
 }
