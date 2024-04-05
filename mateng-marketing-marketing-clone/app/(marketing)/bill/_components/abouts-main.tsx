eea23ea85615503593f7a4bffa2de6783d6651f4
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-05 09:14:11 +0530

    Update abouts-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/bill/_components/abouts-main.tsx
@@ -4,12 +4,12 @@ import { Content } from "./content";
 
 export function AboutsMain() {
   return (
-    <div className="text-center pt-30 pb-50 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       {/* Google Sheets iframe */}
-      <div className="border border-gray-400 rounded-lg p-4 w-full h-screen">
+      <div className="border border-gray-400 rounded-lg p-4 w-full max-w-screen-lg">
         <iframe
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqhJ96Q5S2xKtvtwjqmytwZeOCZnkBBOP7eJfMgVmoIHbg1LA-V9YhCocI2_u7cwkRPvTqPcep34yx/pubhtml"
-          className="w-full h-full"
+          className="w-full h-96" // Set height as needed
           style={{ border: "none" }} // Hide iframe border
           sandbox="allow-scripts allow-same-origin"
         ></iframe>
